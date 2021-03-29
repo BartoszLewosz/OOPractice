566cb522cbac6c00280074d9fe2d9d4d5aef2627
Bartosz <bartosz.lewosz@gmail.com>
2021-03-29 20:11:19 +0100

    Inheritance example with super() method

--- day_01.py
+++ day_01.py
@@ -5,9 +5,17 @@ class Employee:
         self.start_date = start_date
     
     def show_full_data(self):
-        return f"ID: {self.emp_id}\nName: {self.name}\nStart day: {self.start_date}"
+        return f"ID: {self.emp_id} \nName: {self.name} \nStart day: {self.start_date}"
 
 emp_1 = Employee(101, "John", "01/03/2010")
 
 print(emp_1.show_full_data())
 
+class CustomerAssistant(Employee):
+    def __init__(self, emp_id, name, start_date, week_hours):
+        super().__init__(emp_id, name, start_date)
+        self.week_hours = week_hours
+
+emp_2 = CustomerAssistant(102, "Jane", "02/03/2010", 25)
+print(emp_2.show_full_data())
+print(emp_2.week_hours)
