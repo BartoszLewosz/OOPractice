160e1712d742adfdb01ff8d7180791c730aa2180
Bartosz <bartosz.lewosz@gmail.com>
2021-03-29 18:26:27 +0100

    Create constructor

--- day_01.py
+++ day_01.py
@@ -1,2 +1,10 @@
 class Employee:
-    pass
+    def __init__(self, emp_id, name, start_date):
+        self.emp_id = emp_id
+        self.name = name
+        self.start_date = start_date
+
+emp_1 = Employee(101, "John", "01/03/2010")
+
+print(emp_1.name)
+
