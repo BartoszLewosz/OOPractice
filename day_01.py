dd01d416f11e4b3d124ed5edc122598f24410bed
Bartosz <bartosz.lewosz@gmail.com>
2021-03-29 18:29:16 +0100

    create a method to show all Employee data

--- day_01.py
+++ day_01.py
@@ -3,8 +3,11 @@ class Employee:
         self.emp_id = emp_id
         self.name = name
         self.start_date = start_date
+    
+    def show_full_data(self):
+        return f"ID: {self.emp_id}\nName: {self.name}\nStart day: {self.start_date}"
 
 emp_1 = Employee(101, "John", "01/03/2010")
 
-print(emp_1.name)
+print(emp_1.show_full_data())
 
