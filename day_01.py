902703b27b300403e6ede379c2f647185e4bc6a7
Bartosz <bartosz.lewosz@gmail.com>
2021-03-30 13:54:58 +0100

    Create class variable

--- day_01.py
+++ day_01.py
@@ -1,4 +1,7 @@
 class Employee:
+
+    annual_payraise = 1.04
+
     def __init__(self, emp_id, name, start_date):
         self.emp_id = emp_id
         self.name = name
@@ -10,6 +13,7 @@ class Employee:
 emp_1 = Employee(101, "John", "01/03/2010")
 
 print(emp_1.show_full_data())
+print("Annual payrise:" + str(emp_1.annual_payraise))
 
 class CustomerAssistant(Employee):
     def __init__(self, emp_id, name, start_date, week_hours):
@@ -19,3 +23,4 @@ class CustomerAssistant(Employee):
 emp_2 = CustomerAssistant(102, "Jane", "02/03/2010", 25)
 print(emp_2.show_full_data())
 print(emp_2.week_hours)
+print("Annual payrise:" + str(emp_2.annual_payraise))
