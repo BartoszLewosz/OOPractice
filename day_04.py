52003f255a83782f68096b91e9fe879bfe31ad02
Bartosz <bartosz.lewosz@gmail.com>
2021-04-02 13:29:42 +0100

    Add __repr__ method to other classes

--- day_04.py
+++ day_04.py
@@ -26,8 +26,8 @@ class Employee:
     # def __str__(self):
     #     return f"{self.emp_id} {self.name}"
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.emp_id})"
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.emp_id} - {self.name} - {self.start_date} - {self.pay})"
 
 
 emp_1 = Employee(101, "John", "01/03/2010", 9.5)
@@ -43,6 +43,13 @@ class Manager(Employee):
         else:
             self.employees = employees
 
+    def show_employees_id(self):
+        for e_id in self.employees:
+            print(f"{e_id.emp_id}")
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(emp_id: {self.emp_id}, Name: {self.name}, Start date: {self.start_date}, Pay: {self.pay}, Employees: {self.employees})"
+
 
 mngr_1 = Manager(201, "Mark", "01/02/1999", 50000, [emp_1])
 
@@ -53,6 +60,8 @@ class StoreManager(Employee):
     def __init__(self, emp_id: int, name: str, start_date: str, pay: int):
         super().__init__(emp_1, name, start_date, pay)
 
+    # __repr__ as Employee
+
 
 class DeputyManager(Employee):
     """ Creates a deputy manager of company """
@@ -60,12 +69,16 @@ class DeputyManager(Employee):
     def __init__(self, emp_id: int, name: str, start_date: str, pay: int):
         super().__init__(emp_1, name, start_date, pay)
 
+    # __repr__ as Employee
+
 
 class ShiftManager(Employee):
     """ Creates a shift manager of company """
 
     def __init__(self, emp_id: int, name: str, start_date: str, pay: int):
         super().__init__(emp_1, name, start_date, pay)
+
+    # __repr__ as Employee
 
 
 class CustomerAssistant(Employee):
@@ -75,9 +88,12 @@ class CustomerAssistant(Employee):
         super().__init__(emp_id, name, start_date, pay)
         self.week_hours = week_hours
 
-
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(emp_id:{self.emp_id}, name:{self.name}, start_date:{self.start_date}, pay per hour:{self.pay}, week_hours:{self.week_hours})"
 emp_2 = CustomerAssistant(102, "Jane", "02/03/2010", 9.5, 25)
 
 
+# print(mngr_1)
+
+print(emp_2)
 print(mngr_1)
-mngr_1
