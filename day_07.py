ef3297af9696c1c9f3165c0fe15ae63407cd8ec0
Bartosz <bartosz.lewosz@gmail.com>
2021-04-05 17:16:38 +0100

    Add property decorators to getter and setter

--- day_07.py
+++ day_07.py
@@ -1,5 +1,5 @@
 class Celssius:
-    def __init__(self, temperature):
+    def __init__(self, temperature=0):
         ''' The actual temperature value is stored in _temperature variable.
         The temperature attribute is a property object which provides 
         an interface to this private variable. '''
@@ -9,21 +9,26 @@ class Celssius:
         return (self.temperature * 1.8) + 32
 
     # getter
-    def get_temperature(self):
+    #def get_temperature(self):
+    @property
+    def temperature(self):
         print("getting temperature...")
         return self._temperature # '_' in Python means to set as private - not accesible from outside the class
 
     # setter
-    def set_temperature(self, value):
+    #def set_temperature(self, value):
+    @temperature.setter
+    def temperature(self, value):
         print("setting temperature...")
         if value < -273.15:
             raise ValueError("It's impossible!!")
         self._temperature = value
 
-    temperature = property(get_temperature, set_temperature)
+    # temperature = property(get_temperature, set_temperature)
 
 human = Celssius(37)
+print(human.temperature)
 # print(human.get_temperature()) # this line had to change from temperature to get_temperature()
-# print(human.to_fahrenheit())
+print(human.to_fahrenheit())
 
-print(Celssius.__init__.__doc__)
+too_cold = Celssius(-300)
