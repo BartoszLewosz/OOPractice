050d44d378db4c9f6782bce16b3ebbb102ba5eb4
Bartosz <bartosz.lewosz@gmail.com>
2021-04-07 14:47:40 +0100

    Mocking with abstract class

---
+++ day_10.py
@@ -0,0 +1,20 @@
+class Base:
+    def foo(self):
+        print("foo method from Base class")
+        raise NotImplementedError()
+
+    def bar(self):
+        print("bar method from Base class")
+        raise NotImplementedError()
+
+class Concrete(Base): # instantiate/inherit from Base class
+    def foo(self):
+        print("foo method from Concrete(Base)")
+        
+b = Base()
+# b.foo()
+# b.bar()
+
+c = Concrete()
+c.foo()
+c.bar()
