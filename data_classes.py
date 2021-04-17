770867639b4f6f62634ea8fb017e0be82f451012
Bartosz <bartosz.lewosz@gmail.com>
2021-04-17 18:07:38 +0100

    Calculate distance petween two positions

--- data_classes.py
+++ data_classes.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from math import asin, cos, radians, sin, sqrt
 
 @dataclass
 class Position:
@@ -6,8 +7,20 @@ class Position:
     lon: float = 0.0
     lat: float = 0.0
 
-pos_1 = Position('Oslo', 10.8, 59.9)
-pos_2 = Position('somewhere')
+    def distance_to(self, other):
+        r = 6371
+        lam_1, lam_2 = radians(self.lon), radians(other.lon)
+        phi_1, phi_2 = radians(self.lat), radians(other.lat)
+        h = (sin((phi_2 - phi_1) / 2)**2 + cos(phi_1) * cos(phi_2) * sin((lam_2 - lam_1) / 2)**2)
+        return 2 * r * asin(sqrt(h))
 
+
+oslo = Position('Oslo', 10.8, 59.9)
+vancouver = Position('V', -123.1, 49.3)
+pos_1 = Position('Oslo', 10.75, 59.91)
+pos_2 = Position('somewhere')
+pos_3 = Position('Gdansk', 54.35, 18.64)
 print(pos_1)
 print(pos_2)
+print(vancouver.distance_to(pos_3))
+
