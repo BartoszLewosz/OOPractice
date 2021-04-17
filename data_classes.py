31d20c8b6252bf587a0700c525c8331e7e572e21
Bartosz <bartosz.lewosz@gmail.com>
2021-04-17 17:32:45 +0100

    set default values for lon and lat parameters

--- data_classes.py
+++ data_classes.py
@@ -3,9 +3,11 @@ from dataclasses import dataclass
 @dataclass
 class Position:
     name: str
-    lon: float
-    lat: float
+    lon: float = 0.0
+    lat: float = 0.0
 
 pos_1 = Position('Oslo', 10.8, 59.9)
+pos_2 = Position('somewhere')
 
 print(pos_1)
+print(pos_2)
