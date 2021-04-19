d78c4840f20d9f59dd911c39f75a17c4eb80e304
Bartosz <bartosz.lewosz@gmail.com>
2021-04-19 16:37:32 +0100

    Use field() specifier to use 'make_french_deck' function as (mutable) default value

--- DataClassCard.py
+++ DataClassCard.py
@@ -1,25 +1,35 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
+
 
 @dataclass
 class PlayingCard:
     rank: str
     suit: str
 
+
+# def make_french_deck():
+#     return [PlayingCard(r, s) for s in SUITS for r in RANKS]
+
+
 @dataclass
 class Deck:
-    cards: List[PlayingCard]
+    # cards: List[PlayingCard]
 
     def make_french_deck():
         return [PlayingCard(r, s) for s in SUITS for r in RANKS]
 
+    cards: List[PlayingCard] = field(default_factory=make_french_deck)
+
+
 RANKS = '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(',')
-SUITS = '\u2660 \u2665 \u2663 \u2660'.split() 
+SUITS = '\u2660 \u2665 \u2663 \u2660'.split()
 
 queen_of_hearts = PlayingCard('Q', 'hearts')
 ace_of_spades = PlayingCard('A', 'spades')
 two_cards_deck = Deck([queen_of_hearts, ace_of_spades])
 
 french_deck = Deck.make_french_deck()
-#print(two_cards_deck)
-print(french_deck)
+# print(two_cards_deck)
+# print(french_deck)
+print(Deck())
