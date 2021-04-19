491b5957748d0c88b798d0eba5ed277db198b037
Bartosz <bartosz.lewosz@gmail.com>
2021-04-19 15:52:57 +0100

    create Deck class and two_cards_deck object

--- DataClassCard.py
+++ DataClassCard.py
@@ -1,6 +1,17 @@
 from dataclasses import dataclass
+from typing import List
 
 @dataclass
-class DataClassCard:
+class PlayingCard:
     rank: str
     suit: str
+
+@dataclass
+class Deck:
+    cards: List[PlayingCard]
+
+queen_of_hearts = PlayingCard('Q', 'hearts')
+ace_of_spades = PlayingCard('A', 'spades')
+two_cards_deck = Deck([queen_of_hearts, ace_of_spades])
+
+print(two_cards_deck)
