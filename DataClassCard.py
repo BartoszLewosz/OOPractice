b60c97439a928c7b326c8a4d172e89e757e0ac02
Bartosz <bartosz.lewosz@gmail.com>
2021-04-19 16:16:55 +0100

    Specify different ranks and suits. Add method to create a list of rench deck cards (52)

--- DataClassCard.py
+++ DataClassCard.py
@@ -10,8 +10,16 @@ class PlayingCard:
 class Deck:
     cards: List[PlayingCard]
 
+    def make_french_deck():
+        return [PlayingCard(r, s) for s in SUITS for r in RANKS]
+
+RANKS = '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(',')
+SUITS = '\u2660 \u2665 \u2663 \u2660'.split() 
+
 queen_of_hearts = PlayingCard('Q', 'hearts')
 ace_of_spades = PlayingCard('A', 'spades')
 two_cards_deck = Deck([queen_of_hearts, ace_of_spades])
 
-print(two_cards_deck)
+french_deck = Deck.make_french_deck()
+#print(two_cards_deck)
+print(french_deck)
