c21829180767191885ce5f1580d1b59028d8f91f
Bartosz <bartosz.lewosz@gmail.com>
2021-04-20 20:14:23 +0100

    Comparing cards feature (method

--- DataClassCard.py
+++ DataClassCard.py
@@ -2,10 +2,19 @@ from dataclasses import dataclass, field
 from typing import List
 
 
+RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+SUITS = '\u2660 \u2665 \u2663 \u2666'.split()
+
+
 @dataclass(order=True)
 class PlayingCard:
+    sort_index: int = field(init=False, repr=False)
     rank: str
     suit: str
+
+    def __post_init__(self):
+        self.sort_index = (RANKS.index(self.rank) *
+                           len(SUITS) + SUITS.index(self.suit))
 
     def __str__(self):
         return f'{self.suit}{self.rank}'
@@ -20,25 +29,25 @@ class Deck:
 
     cards: List[PlayingCard] = field(default_factory=make_french_deck)
 
-    # def __repr__(self):
-    #     cards = ', '.join(f'{c!s}' for c in self.cards)
-    #     return f'{__class__.__name__}({cards})'
+    def __repr__(self):
+        cards = ', '.join(f'{c!s}' for c in self.cards)
+        return f'{__class__.__name__}({cards})'
 
 
-RANKS = '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(',')
-SUITS = '\u2660 \u2665 \u2663 \u2660'.split()
+# queen_of_hearts = PlayingCard('Q', 'hearts')
+# ace_of_spades = PlayingCard('A', 'spades')
+# two_cards_deck = Deck([queen_of_hearts, ace_of_spades])
 
-queen_of_hearts = PlayingCard('Q', 'hearts')
-ace_of_spades = PlayingCard('A', 'spades')
-two_cards_deck = Deck([queen_of_hearts, ace_of_spades])
-
-french_deck = Deck.make_french_deck()
+# french_deck = Deck.make_french_deck()
 # print(str(queen_of_hearts))
-card = PlayingCard('Q', '\u2665')
+card_1 = PlayingCard('Q', '\u2665')
+card_2 = PlayingCard('A', '\u2666')
 # print(two_cards_deck)
 # print(french_deck)
 # print(Deck())
-# print(card)
-print(queen_of_hearts)
-print(ace_of_spades)
-print(ace_of_spades > queen_of_hearts)
+print(card_1)
+print(card_2)
+print(card_1 < card_2)
+# print(queen_of_hearts)
+# print(ace_of_spades)
+# print(ace_of_spades > queen_of_hearts)
