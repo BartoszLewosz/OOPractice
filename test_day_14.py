07db844fc9bad544a2cba731a03531293e1583d2
Bartosz <bartosz.lewosz@gmail.com>
2021-04-28 17:37:08 +0100

    Rename TestClass to TestClassEmployee

--- test_day_14.py
+++ test_day_14.py
@@ -2,7 +2,7 @@ import pytest
 from day_13 import *
 
 
-class TestClass:
+class TestClassEmployee:
     @pytest.fixture
     def emp_1(self):
         emp_1 = Employee(101, 'John', '01/02/2016', 2016, 9.5)
@@ -10,14 +10,12 @@ class TestClass:
 
     @pytest.fixture
     def mngr_1(self, emp_1):
-        mngr_1 = Manager(202, 'Daniel', '12/12/12', 2010, 11.5, [emp_1])  
+        mngr_1 = Manager(202, 'Daniel', '12/12/12', 2010, 11.5, [emp_1])
         return mngr_1
 
     def test_use_headset(self, emp_1):
-        assert emp_1.use_headset('message from emp_1') == "John says on headset: 'message from emp_1'"
+        assert emp_1.use_headset(
+            'message from emp_1') == "John says on headset: 'message from emp_1'"
 
     def test_if_emp_1_is_subordinate_to_mngr_1(self, emp_1, mngr_1):
         assert emp_1 in mngr_1.employees == [emp_1]
-
-# def test_if_emp_1_is_subordinate_to_mngr_1(emp_1, mngr_1):
-#     assert emp_1 in mngr_1.employees == [emp_1]
