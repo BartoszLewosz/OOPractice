0d31467153824e160369cdfb534fd2648e568d46
Bartosz <bartosz.lewosz@gmail.com>
2021-04-28 13:43:44 +0100

    Create function for pytest.fixture

--- test_day_14.py
+++ test_day_14.py
@@ -1,6 +1,11 @@
 import pytest
 from day_13 import *
 
+def emp_1():
+    emp_1 = Employee(101, 'John', '01/02/2016', 2016, 9.5)
+    return emp_1
+    pass
+
 def test_use_headset():
     emp_1 = Employee(101, 'John', '01/02/2016', 2016, 9.5)
     assert emp_1.use_headset('message from emp_1') == "John says on headset: 'message from emp_1'"
