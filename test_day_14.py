6aa720abc5357a9099ea3aa03a9b23ff98822fcf
Bartosz <bartosz.lewosz@gmail.com>
2021-04-28 16:11:06 +0100

    Decorate mngr_1 as fixture

--- test_day_14.py
+++ test_day_14.py
@@ -6,6 +6,7 @@ def emp_1():
     emp_1 = Employee(101, 'John', '01/02/2016', 2016, 9.5)
     return emp_1
 
+@pytest.fixture
 def mngr_1():
     mngr_1 = Manager(202, 'Daniel', '12/12/12', 2010, 11.5, [emp_1])
     return mngr_1
